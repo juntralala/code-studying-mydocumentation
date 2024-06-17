cb8f557639e9294d0ba249bee87f4d02ce6c6629
Muhammad Junaidi <ujun.muhammadjunaidi@gmail.com>
2024-06-17 22:03:05 +0800

    Modified Javascript/Dasar/Function.js

--- Javascript/Dasar/function.js
+++ Javascript/Dasar/function.js
@@ -1,19 +1,3 @@
 function sapa() {
     console.log("Halo semuanya")
 }
-
-function sapaOrang(namaOrang) {
-console.log(`Hai ${namaOrang}`)
-}
-
-function tambah(a,b) {
-    let hasil = a+b
-    return hasil 
-}
-
-sapa()
-
-sapaOrang("Muhammad Junaidi")
-
-let hasilTambah = tambah(10,10)
-console.log(hasilTambah)
